39ae685771f4fd8e7cba81fba0d68767f782dfd1
joaovpinheiro <joaovpinheiros@gmail.com>
2024-11-24 11:12:07 -0300

    deixar imagenm clara quando o jogo não estiver alugado

--- js/app.js
+++ js/app.js
@@ -1,15 +1,22 @@
 
+// Mudar status de aluguel  
 function alterarStatus(id){
+  // Game que quermos modificar
   const game = document.getElementById(`game-${id}`)
-  const btn = game.querySelector('a');
+  // Botão do elemento que queremos modificar
+  const imagem  = game.querySelector('.dashboard__item__img');
+  const btn = game.querySelector('.dashboard__item__button');
+
 
   if(btn.classList.contains('dashboard__item__button--return')){
     btn.classList.remove('dashboard__item__button--return');
+    imagem.classList.remove('dashboard__item__img--rented');
     btn.innerHTML = 'Alugar';
   }
   else{
     btn.classList.add('dashboard__item__button--return');
+    imagem.classList.add('dashboard__item__img--rented');
+    
     btn.innerHTML = 'Devolver';
   }
-
 }
